cf17295dafb7513d32d9989700243ea2b5a8e64d
akashp69 <akashpakhare499@gmail.com>
2022-11-23 23:46:09 +0530

    Added:Program For Player rolls the die to get a number between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
 
-        System.out.println("Snake and Ladder game played with single player at start position 0");
-        int position=0;
-        System.out.println("Current position is :" + position);
+        int checkPosition =(int) Math.floor(Math.random() * 10) %6 +1;
+
+        System.out.println("Current position is :" + checkPosition);
     }
 }
