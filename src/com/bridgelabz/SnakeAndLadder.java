a2dc2eebd1e21f6d1cb5ba4afb49ff0f6a17e67e
akashp69 <akashpakhare499@gmail.com>
2022-11-23 23:55:30 +0530

    Added:Program For Player checks  Option for No play, Ladder and Snake

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,25 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
-
+        int currentPosition=43;
+        int snake= (int) Math.floor(Math.random() * 10) %3;
         int checkPosition =(int) Math.floor(Math.random() * 10) %6 +1;
+        if(snake==1) {
+            currentPosition =currentPosition + checkPosition;
+            System.out.println("Ladder Moves Ahead");
+        }
+        else if(snake==2)  {
+            currentPosition =currentPosition - checkPosition;
+            System.out.println("Snake Moves Behind");
+        }
+        else {
+            currentPosition=currentPosition;
+            System.out.println("No Play");
 
-        System.out.println("Current position is :" + checkPosition);
-    }
-}
+        }
+        System.out.println("current postion after 43 is: " + currentPosition);
+
+    }}
