32cc5ccfe6bc7ac2db38d714f10aa8976edb266f
akashp69 <akashpakhare499@gmail.com>
2022-11-25 00:14:57 +0530

    Added:Program For The Player gets to exact winning position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,27 +1,32 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
 
-        int currentPosition=96;
-        int snake= (int) Math.floor(Math.random() * 10) %3;
-        int dieNumber =(int) Math.floor(Math.random() * 10) %6 +1;
-        if(snake==1) {
-            currentPosition=currentPosition + dieNumber;
-            if(currentPosition<=100)
-                System.out.println("Ladder Option Moves Ahead " +currentPosition );
-        }
-        else if(snake==2)  {
-            currentPosition =currentPosition - dieNumber;
-            System.out.println("Snake Option Moves Behind " +currentPosition );
-        }
-        else {
-            currentPosition=currentPosition;
+        int currentPosition = 96;
+        int snake = (int) Math.floor(Math.random() * 10) % 3;
+        int checkPosition = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        if (snake == 1) {
+            currentPosition = currentPosition + checkPosition;
+            if (currentPosition <= 100)
+                System.out.println("Ladder Option Moves Ahead " + currentPosition);
+        } else if (snake == 2) {
+            currentPosition = currentPosition - checkPosition;
+            System.out.println("Snake Option Moves Behind " + currentPosition);
+        } else {
+            currentPosition = currentPosition;
             System.out.println("No Play");
         }
-        if(currentPosition<=100)
-            System.out.println("current postion after roll die is: " + currentPosition);
+        if (currentPosition <= 100)
+            System.out.println("current postion after die throw is: " + currentPosition);
         else
             System.out.println("Throw Die Again");
+        while (currentPosition==100){
+            System.out.println("Game Win");
+            break;
+        }
+    }
 
-    }}
+}
